f9897fbd672fb46e671a185e0cdcd333a4e407fd
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-11 21:48:36 +0530

    Updated!

---
+++ P3_nextLine.py
@@ -0,0 +1,9 @@
+
+
+# next line (\n) in C/C++ is needed , but in python it not needed , it automatically identifes and seperates the line of code 
+
+print("Hello ")
+
+print("Hi")
+
+print("12334555")
