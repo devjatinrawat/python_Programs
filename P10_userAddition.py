e807611544f47ee32c1d3b3a6b979887519cd97f
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-11 22:49:05 +0530

    Updated!

---
+++ P10_userAddition.py
@@ -0,0 +1,21 @@
+
+
+'''
+num1 = input("Enter first number : ")
+num2 = input("Enter Second Number : ")
+sum = num1 + num2
+print("The Sum is " +sum )
+
+this will give output 1020 and not 30 , Why? becoz we have not mentioned data type 
+
+
+IMP
+An Interviewer’s Favorite Question: “How Are Python Strings Stored in Internal Memory”
+'''
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+ 
+sum = num1 + num2
+ 
+print("The Sum Is :", sum)
