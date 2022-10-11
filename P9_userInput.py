71a6b53e2ebd7a0289ffadc8389d594b350823a7
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-11 22:37:38 +0530

    Updated!

---
+++ P9_userInput.py
@@ -0,0 +1,8 @@
+
+
+
+# User Input command to take input in Python , cin>> , scanf() , input("Enter Int : ") == readline('Enter Int: ') (Very Very Similar In PHP), 
+
+userInput = input("Enter Any Integer : ")
+#no comma , to seperate
+print("User Entered : " +userInput )
