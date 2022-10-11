6fff427d0b2780548afbea063474873e2907a1e8
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-11 22:23:10 +0530

    Updated!

---
+++ P7_escapeSequence.py
@@ -0,0 +1,26 @@
+
+'''
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+
+
+'''
+
+txt = 'It\'s alright.'
+print(txt) 
+
+txt = "This will insert one \\ (backslash)."
+print(txt) 
+
+
+
+
+
+
