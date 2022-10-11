013e774eac0414b7abf561c91d49d77a0423fd8d
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-11 22:06:51 +0530

    Updated!

---
+++ P6_indentation.py
@@ -0,0 +1,21 @@
+
+
+'''
+Indentation refers to the spaces at the beginning of a code line.
+
+Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+
+Python uses indentation to indicate a block of code.
+
+Indentation == Scooping 
+
+https://www.w3schools.com/python/gloss_python_indentation.asp
+'''
+
+if (5 > 2):
+  print("Five is greater than two!")
+  
+  
+#   if 5 > 2:
+# print("Five is greater than two!")
+# is WRONG , Syntax ERROR becoz no indentation
