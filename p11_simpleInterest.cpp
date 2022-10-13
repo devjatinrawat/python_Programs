fcd2da5966beee366e0f5c0bd7c3699fea6a7b30
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-13 23:37:57 +0530

    Updated!

---
+++ p11_simpleInterest.cpp
@@ -0,0 +1,11 @@
+
+
+# Python Program To Cal SI = P*R*T/100
+
+p = int(input("Enter Principle Interest: "))
+r = int(input("Enter Principal Rate : "))
+t = int(input("Enter principle Time: "))
+
+si = (p*r*t)/(100)
+
+print("The Simple Interest is : ",si)
